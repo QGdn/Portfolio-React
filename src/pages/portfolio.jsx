383d1b2532c6453f99a79511ef475bb9170b3541
QGdn <quentin.gaudin78@gmail.com>
2024-11-25 21:22:08 +0100

    Fix errors / CSS

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -16,7 +16,7 @@ function Card(props) {
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.description}</p>
-                <a href="#" className="btn btn-primary">Voir le site</a>
+                <a href="/portfolio" className="btn btn-primary">Voir le site</a>
             </div>
             <div className='card-footer'>
                 <small className='text-body-secondary'>{props.footer}</small>
@@ -31,7 +31,7 @@ export default function Portfolio() {
             <img src={Banner} alt="Bannière bleue" className='banner'/>
             <section className="container title">
                 <h1>Portfolio</h1>
-                <p>Voici quelques-unes de mes réalisations</p>
+                <p>Voici quelques-unes de mes réalisations.</p>
             </section>
             <section className="container cards">
                 <Card img={FreshFood} alt="Image d'un panier de nourriture fraiche" title="Fresh Food" description="Site de vente de produits frais en ligne" footer="Site réalisé avec PHP et MySQL"/>
@@ -42,7 +42,7 @@ export default function Portfolio() {
 
                 <Card img={SEO} alt="Regroupement de mots lié au Web" title="SEO" description="Amélioration du référencement d'un site e-commerce" footer="Utilisation des outils SEO"/>
 
-                <Card img={API} alt="Image montrant de la programmation sur un écran" title="Création d'une API" description="Création d'une API RESTFULL publique" footer="PHP - SYMPHONY"/>
+                <Card img={API} alt="Image montrant de la programmation sur un écran" title="Création d'une API" description="Création d'une API RESTFULL publique" footer="PHP - SYMFONY"/>
 
                 <Card img={Maquette} alt="Bureau en bois avec une tablette et deux ordinateurs" title="Maquette d'un site web" description="Création du prototype d'un site" footer="Réalisé avec FIGMA"/>
             </section>
